486aecb62d884d7e0a4868ddf77a0471546ec42c
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-06-21 11:03:34 +0100

    add props

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import './App.css';
+import "./App.css";
 import NavBar from "./Components/NavBar";
-import {BrowserRouter,Routes,Route,} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Card from "./Components/Card";
 
 function App() {
   return (
     <div className="App">
-       <BrowserRouter>
-       {/* <NavBar/> */}
+      <BrowserRouter>
         <Routes>
-          <Route path="/" element={<NavBar /> }/>
-          
+          <Route path="/" element={<NavBar />} />
         </Routes>
       </BrowserRouter>
-     
+      <Card  currency = "$" price ={156} country="USA" rating = {5.0} available={6}/>
     </div>
   );
 }
