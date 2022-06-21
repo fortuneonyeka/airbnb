8704637045e71160319a5a9652da0184c00781c5
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-06-21 20:40:39 +0100

    passing data through props

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import "./App.css";
 import NavBar from "./Components/NavBar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Card from "./Components/Card";
+import experiencedata from "./Data/Data"
 
 function App() {
+  const card = experiencedata.map((e) => {
+    return <Card title={e.title} location={e.location} openSpot={e.openSpots}   price ={e.price} country="USA" rating = {e.stats.rating} reviewCount={e.stats.reviewCount}  description={e.description} img={e.coverImg}/>
+  });
   return (
     <div className="App">
       <BrowserRouter>
@@ -11,7 +15,8 @@ function App() {
           <Route path="/" element={<NavBar />} />
         </Routes>
       </BrowserRouter>
-      <Card  currency = "$" price ={156} country="USA" rating = {5.0} available={6}/>
+      {card}
+      {/* <Card  currency = "$" price ={156} country="USA" rating = {5.0} reviewCount={6} title="Life lessons with Katie Zaferes" wedding="Learn wedding photography"/> */}
     </div>
   );
 }
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,55 +1,27 @@
 import React from "react";
-import Image from "../assets/images/image 12.png";
-import Mountain from "../assets/images/mountain-bike 1.png";
-import Wedding from "../assets/images/wedding-photography 1.png";
-import Star from "../assets/images/Star 1.png";
+
 import "./style.css";
 
-const Card = ({price,country, rating, currency,available}) => {
+const Card = ({price,location, openSpot, rating, currency,reviewCount,title, img, description}) => {
   return (
-    <div>
-      <div className="card">
-        <div>
-          <img className="imgs" src={Image} alt="" />
-          <div className="card-stats">
-            <img className="star" src={Star} />
-            <span>{rating}</span>
-            <span className="gray">{available}</span>
-            <span className="gray">{country}</span>
-          </div>
-          <div className="content">
-            <span>Life lessons with Katie Zaferes</span>
-            <h4 className="price">From {currency}{price} / <span className="gray">person</span></h4>
-          </div>
-        </div>
-        <div>
-          <img className="imgs" src={Wedding} alt="" />
-          <div className="card-stats">
-            <img className="star" src={Star} />
-            <span>5.0</span>
-            <span className="gray">{available}</span>
-            <span className="gray">{country}</span>
-          </div>
-          <div className="content">
-            <span>Learn wedding photography</span>
-            <h4 className="price">From {currency}{price} / <span className="gray">person</span></h4>
-          </div>
-        </div>
-        <div>
-          <img className="imgs" src={Mountain} alt="" />
-          <div className="card-stats">
-            <img className="star" src={Star} />
-            <span>{rating}</span>
-            <span className="gray"> { available}</span>
-            <span className="gray">{country}</span>
-          </div>
 
+      <div className="container">
+        <div className="card">
+          <img className="imgs" src={img} alt="" />
+          <div className="card-stats">
+            {/* <img className="star" src={Star} /> */}
+            <span>Open Spots{openSpot}</span>
+            <span>Ratings:{rating}</span>
+            <span className="gray">Reviews:({reviewCount})</span>
+            <span className="gray">Location:{location}</span>
+          </div>
           <div className="content">
-            <span>Group Mountain Biking</span>
+            <span>{title}</span>
+            <span className="description">{description}</span>
             <h4 className="price">From {currency}{price} / <span className="gray">person</span></h4>
           </div>
         </div>
-      </div>
+       
     </div>
   );
 };
