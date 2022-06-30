cbfb30b696c1e6fbf4d77a8b9cc5368f37f0c1d4
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-06-30 10:08:48 +0100

    changed method of sending props

--- src/App.js
+++ src/App.js
@@ -6,7 +6,16 @@ import experiencedata from "./Data/Data"
 
 function App() {
   const card = experiencedata.map((e) => {
-    return <Card title={e.title} location={e.location} openSpot={e.openSpots}   price ={e.price} country="USA" rating = {e.stats.rating} reviewCount={e.stats.reviewCount}  description={e.description} img={e.coverImg}/>
+    // return <Card key={e.id}  title={e.title} location={e.location} openSpot={e.openSpots}   price ={e.price} country="USA" rating = {e.stats.rating} reviewCount={e.stats.reviewCount}  description={e.description} img={e.coverImg}
+    // />
+    // OR
+    // Take the entire result of the map which is e as props
+    // return <Card key={e.id} e={e}/>
+    // with this, you would access the props as props.e.name of the item like props.e.location
+    // OR
+    // spreed the entire result of the map which is e as props
+    // With the spreed, you would access props with with props.variable name like props.location
+    return <Card key={e.id} {...e}/>
   });
   return (
     <div className="App">
@@ -15,7 +24,8 @@ function App() {
           <Route path="/" element={<NavBar />} />
         </Routes>
       </BrowserRouter>
-      {card}
+      <section className="cards-list">{card}</section>
+      
       {/* <Card  currency = "$" price ={156} country="USA" rating = {5.0} reviewCount={6} title="Life lessons with Katie Zaferes" wedding="Learn wedding photography"/> */}
     </div>
   );
